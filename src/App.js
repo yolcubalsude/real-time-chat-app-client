247ada53759a11e0fbb9444e21312b1df3bb9653
yolcubalsude <sude.yolcubal@gmail.com>
2025-08-11 10:35:39 +0300

    Add react-scroll-to-bottom for improved chat experience; update App and Chat components for better UI flow

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
-import Chat from './Chat';
-import React from "react";
+import "./App.css";
+import io from "socket.io-client";
 import { useState } from "react";
-import './App.css';
-import { io } from "socket.io-client";
-const socket = io("http://localhost:3001");
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
- 
 
   return (
     <div className="App">
-      <h3>Join A Chat</h3>
-        <input type="text" 
-        placeholder="Room ID..." 
-        onChange={(event) => {
-          setRoom(event.target.value)
-          }}
-          value={room}
-          />
-
-          <input type="text" 
-          placeholder="Username..." 
-          onChange={(event) => {
-            setUsername(event.target.value)
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
             }}
-            value={username}
-            />
-        <button onClick={joinRoom}>Join A Room</button>
-        <Chat socket={socket} username={username} room={room}/>
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </div>
-  ); 
+  );
 }
 
 export default App;
--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,5 @@
-
 import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
@@ -23,34 +23,49 @@ function Chat({ socket, username, room }) {
     }
   };
 
-
-
-  useEffect(() => { 
-    socket.on("receive_message", (data) =>  {
-console.log(data);
-    })
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
   }, [socket]);
 
-
-
-
-
-
   return (
-    <div>
+    <div className="chat-window">
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
-
-      <div className="chat-body"></div>
-
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
       <div className="chat-footer">
         <input
           type="text"
-          placeholder="Message..."
           value={currentMessage}
+          placeholder="Hey..."
           onChange={(event) => {
             setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
           }}
         />
         <button onClick={sendMessage}>&#9658;</button>
